932ed354220909bb7adb2301f5562e718c194ee2
blupie01 <jeffrey.chou.89@gmail.com>
2017-02-15 15:08:41 -0500

    database/table setup and console logging prod_ID/quant

---
+++ bamazonCustomer.js
@@ -0,0 +1,195 @@
+// If not, the app should log a phrase like Insufficient quantity!, 
+// and then prevent the order from going through.
+// However, if your store does have enough of the product, 
+// you should fulfill the customer's order.
+
+// This means updating the SQL database to reflect the remaining quantity.
+// Once the update goes through, show the customer the total cost of their purchase.
+// Also insert a record into the sales table with the product_id and quantity 
+// purchased and current timestamp
+
+//----------------------------------------------------------------------------------------------
+
+var mysql = require('mysql');
+var inquirer = require('inquirer');
+
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'Avernus#1',
+  database : 'bamazon_db',
+});
+
+connection.connect();
+
+// Running this application will first display all of the items available for sale. 
+// Include the ids, names, and prices of products for sale.
+var startOrder = function() {
+
+	connection.query("SELECT id, product_name, price FROM products", function (error, results) 
+	{
+		// output results into terminal cleanly.
+		for (var i = 0; i < results.length; i++) {
+			var productID = results[i].id;
+			var productName = results[i].product_name;
+			var price = results[i].price;
+			var productObj = {id: productID, product_name: productName, price: price};
+
+			console.log("Product ID: " + productID + " Product Name: " + productName + " Price: " + price);
+		};
+
+		// begin prompt to take order
+		inquirer.prompt([{
+		type: "input",
+		name: "product_id",
+		message: "Please enter the product ID you would like to purchase: ",
+		validate: function(value) {
+      			if (isNaN(value) === false) {
+        			return true;
+      			}
+      			return false;
+    			}		
+		}, {
+		type: "input",
+		name: "purchase_quant",
+		message: "Quantity you want to purchase: ",
+		validate: function(value) {
+      			if (isNaN(value) === false) {
+        			return true;
+      			}
+      			return false;
+    			}
+		}]).then(function(data) {
+			var id = data.product_id;
+			var quant = data.purchase_quant;
+
+			console.log(id, quant);
+		});
+
+	}
+)};	
+
+startOrder();
+
+// The first should ask them the ID of the product they would like to buy.
+	// The second message should ask how many units of the product they would like to buy.
+	// Once the customer has placed the order, your application should check if your 
+	// store has enough of the product to meet the customer's request.
+		
+// 		inquirer.prompt([{
+// 			type: "input",
+// 			name: "product_id",
+// 			message: "Please enter the product ID you would like to purchase: ",
+// 			validate: function(value) {
+//       					if (isNaN(value) === false) {
+//         					return true;
+//       					}
+//       					return false;
+//     				  }		
+// 		}, {
+// 			type: "input",
+// 			name: "purchase_quant",
+// 			message: "Quantity you want to purchase: ",
+// 			validate: function(value) {
+//       					if (isNaN(value) === false) {
+//         					return true;
+//       					}
+//       					return false;
+//     				  }
+// 		}]).then(function(data) {
+
+// 		});
+//   	});
+// };
+
+
+
+// var updateInventory = function(quantity) {
+// 	connection.query("UPDATE " + table + " SET ? WHERE ?", [{
+// 		name : 'bruno beer'
+// 	  }, {
+// 	  	id : id
+// 	  }], function(err, res) { 
+// 	  	if (err) return console.log(err);
+// 	  	console.log('update completed!')
+// 	  });
+
+	// connection.query("UPDATE products )
+// }
+
+//NOT IN USE---------------------------------------------------------------------------------
+// function selectTable(table){
+// 	connection.query('SELECT * from ' + table, function (error, results, fields) {
+// 	  if (error) throw error;
+// 	  console.log(results);
+// 	});
+// }
+//--------------------------------------------------------------------------------------------
+// connection.query('SELECT * from drankers', function (error, results, fields)
+// {
+// 	console.log(results);
+// 	console.log('\n');
+
+// 	inquirer.prompt([
+// 	{type: "input",
+// 	  name: "dranker_id",
+// 	  message: "Put the id of the dranker you are."}
+// 	]).then(function(data){
+// 		var dranker = data.dranker_id;
+
+// 		connection.query('SELECT * from beers', function (error, results, fields) {
+// 			console.log(results);
+// 			console.log('\n');
+// 			inquirer.prompt([
+// 			{type: "input",
+// 			  name: "beer_id",
+// 			  message: "Put the id of the beer that you want."}
+// 			]).then(function(data){
+// 				//do an insert into mysql 
+// 				connection.query('INSERT into dranken_beers SET ?', {
+// 					beer_id : data.beer_id,
+// 					dranker_id : dranker
+// 				}, function (error, results, fields) {
+// 					console.log('insert complete')
+// 				});
+// 			});
+// 		});
+
+// 	});
+// });
+
+// function insertIntoTable(name, type, abv, table){
+//   connection.query("INSERT INTO " + table + " SET ?", {
+//       name: name,
+//       type: type,
+//       abv: abv
+//     }, function(err, res) { console.log('completed!')});
+// }
+
+// function deleteFromTable(id, table){
+// 	connection.query("DELETE FROM " + table + " WHERE ?", {
+// 	    id: id
+// 	  }, function(err, res) { 
+// 	  	if (err) return console.log(err);
+// 	  	console.log('delete completed!')
+// 	  });
+// }
+
+// //write update function
+// function updateTable(id, table){
+// 	connection.query("UPDATE " + table + " SET ? WHERE ?", [{
+// 		name : 'bruno beer'
+// 	  }, {
+// 	  	id : id
+// 	  }], function(err, res) { 
+// 	  	if (err) return console.log(err);
+// 	  	console.log('update completed!')
+// 	  });
+// }
+
+// //write delete function
+
+
+// // insertIntoTable('beer', 'i dont know beer', 100, 'beers');
+// // deleteFromTable(7, 'beers');
+// updateTable(1, 'beers');
---
+++ bamazon_db.sql/bamazon_db_products.sql
@@ -0,0 +1,56 @@
+-- MySQL dump 10.13  Distrib 5.7.12, for Win64 (x86_64)
+--
+-- Host: localhost    Database: bamazon_db
+-- ------------------------------------------------------
+-- Server version	5.7.16-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `products`
+--
+
+DROP TABLE IF EXISTS `products`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `products` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `product_name` varchar(40) NOT NULL,
+  `department_id` int(11) NOT NULL,
+  `price` decimal(7,2) NOT NULL,
+  `stock_quantity` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `department_id` (`department_id`),
+  CONSTRAINT `products_ibfk_1` FOREIGN KEY (`department_id`) REFERENCES `departments` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `products`
+--
+
+LOCK TABLES `products` WRITE;
+/*!40000 ALTER TABLE `products` DISABLE KEYS */;
+INSERT INTO `products` VALUES (1,'Green Tea',3,4.50,47589),(2,'Jasmine Tea',3,6.95,687989),(3,'Black Tea',3,7.65,5567),(4,'Peppermint Tea',3,4.50,2344),(5,'Barley Tea',3,7.60,350),(6,'Dishonored 2',1,75.00,6879),(7,'For Honor',1,85.00,23455),(8,'Oranges',2,6.95,34532),(9,'Potatoes',2,1.95,23412),(10,'Brussel Sprouts',2,3.95,9987),(11,'Carrots',2,2.95,12345);
+/*!40000 ALTER TABLE `products` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-02-15 13:13:42
